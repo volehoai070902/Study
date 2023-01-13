8954ff7c909abd518366381cb29e9843b2cf70a3
vo le hoai <volehoai070902@gmail.com>
2023-01-14 00:07:32 +0700

    fix

--- test1/app.js
+++ test1/app.js
@@ -5,11 +5,21 @@ const hbs = require("hbs");
 const app = express();
 const mongoose = require("mongoose");
 const router = require("./router/index");
-let passwordDB;
-const password = "lehoai123";
+const passport = require("passport");
+const session = require('express-session');
+const initializePassport = require("./config/passport-config");
+
 app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+initializePassport(passport);
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+  }));
 
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://127.0.0.1:27017/Love", (err)=>{
@@ -18,6 +28,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/Love", (err)=>{
     console.log("connected to database");
 })
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.authenticate
 router(app);
 
 app.listen(3000,()=>{
--- test1/config/passport-config.js
+++ test1/config/passport-config.js
@@ -1,12 +1,23 @@
 ﻿const LocalStrategy = require("passport-local").Strategy;
+const async = require("hbs/lib/async");
+const userModel = require("../model/userModel");
+const bcrypt = require("bcrypt");
+const { use } = require("passport");
 
-
-
-function initialize(passport){
-    const authenticate = async (email,password,done)=>{
-
-    } 
-    passport.use(new LocalStrategy({usernameField:'email'}), authenticate);
-    passport.serializeUser((user,done)=>{});
-    passport.deserializeUser((id, done)=>{});
+async function initialize(passport) {
+    const authenticate = async (email, password, done) => {
+        const user = await userModel.findOne({ email: email })
+        if (!user) { return done(null, false); }
+        if (!bcrypt.compare(password, user.password)) {
+            return done(null,false,{message: "password incorrect"});
+        }
+        return done(null,user);
+    }
+    passport.use(new LocalStrategy({ usernameField: 'email' }), authenticate);
+    passport.serializeUser((user,done)=>{done(null,user._id)});
+    passport.deserializeUser((id, done)=>{
+        done(null, userModel.find({_id:id}));
+    });
 }
+
+module.exports = initialize;
