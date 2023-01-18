9c27c8f9cd7bba7563cdcfb289565953ec300207
Le Hoai <volehoai070902@gmail.com>
2023-01-18 14:27:02 +0700

    admin

---
+++ test1/Admin/app.js
@@ -0,0 +1,18 @@
+﻿const express = require("express");
+const app = express();
+const path = require("path");
+const exphbs = require("express-handlebars").engine;
+
+app.set("views", __dirname + path.join("/views"));
+app.engine('hbs',exphbs({
+    extname:"hbs",
+    defaultLayout:'layouts',
+    partialsDir: __dirname + "/views/partials"
+}))
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.listen(5000);
---
+++ test1/model/productModel.js
@@ -0,0 +1,10 @@
+﻿const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const product = new Schema({
+    id: new Date.now(),
+    name:String,
+    price: Number,
+    image: String,
+    description: String,
+})
